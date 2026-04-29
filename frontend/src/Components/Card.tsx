5f0419c99256cd62473285a9ce182052b29827d1
joaoestevaoMoura <chevetteturbo58@email.com>
2026-04-29 19:33:59 -0300

    Atualização do frontend e novos componentes

---
+++ frontend/src/Components/Card.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Card.css";
+type Props = {}
+
+const Card = (props: Props) => {
+  return (
+    <div className="card">
+      <img
+        src="https://imgs.search.brave.com/33Ml2QSwORUR9WVwcbiwQA9SoHqOpV3UUh39g9TVANs/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzE4LzQxLzM5LzM5/LzM2MF9GXzE4NDEz/OTM5MTJfVzBpYjV3/ZkoyVk53czVjODB3/T3I2U1RZVHRRRUN3/VkcuanBn"
+        alt="Imagem do card"
+      />
+      <div className="details">
+        <h2>AAPL</h2>
+        <p>$110</p>
+      </div>
+      <p className="info">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, molestias!
+      </p>
+    </div>
+  )
+}
+
+export default Card
