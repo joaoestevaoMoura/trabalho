5114fadf3a29ae89870d03e926e60ecec11d77b4
joaoestevaoMoura <chevetteturbo58@email.com>
2026-04-29 22:28:47 -0300

    Atualiza componente Search

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,20 +1,23 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent,SyntheticEvent,useState } from 'react'
 
 type Props = {}
 
 const Search: React.FC<Props> = () => {
   const [search, setSearch] = useState<string>("");
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
-    console.log(e.target.value);
+    console.log(e);
+  };
+  const onclick = (e: SyntheticEvent) =>{
+    console.log(e);
   };
 
   return (
-    <div>
-      <input value={search} onChange={onChange} />
+    <div>  
+      <input value={search} onChange={(e) => handleChange(e)} ></input>
+      <button onClick={(e) =>  onclick(e)}/>
     </div>
   )
 }
-
 export default Search
