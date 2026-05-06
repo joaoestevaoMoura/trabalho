292c57bdf90a6637af483247157002a010fe9f02
joaoestevaoMoura <chevetteturbo58@email.com>
2026-05-06 16:24:02 -0300

    Adicionando p design

---
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Table from '../../Components/Table/Table'
+
+type Props = {}
+
+const DesignPage = (props: Props) => {
+  return (
+    <> 
+    <h1>
+        FinShark Design Page 
+    </h1>
+    <h2> Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam maxime hic architecto placeat inventore reprehenderit praesentium cumque modi recusandae aspernatur molestias ullam nam saepe illum, dicta, nostrum ab tempore provident!</h2>
+     <Table/>
+    </>
+  )
+}
+
+export default DesignPage
