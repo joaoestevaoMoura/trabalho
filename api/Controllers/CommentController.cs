151d80e371f02ce975acd03a26fe751f590962e9
joaoestevaoMoura <chevetteturbo58@email.com>
2026-05-14 15:27:10 -0300

    bando de dados ok

---
+++ api/Controllers/CommentController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Interfaces;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/comment")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentRepository _commentRepository;
+
+        public CommentController(ICommentRepository commentRepo)
+        {
+            _commentRepository = commentRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var comments = await _commentRepository.GetAllAsync();
+
+            var commentDto = comments.Select(s => s.ToCommentDto());
+
+            return Ok(commentDto);
+        }
+    }
+}
